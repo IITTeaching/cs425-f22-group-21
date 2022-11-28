29746bab6d1d1587663e5b203208243acbd98e91
Erik Pacheco <92812258+Erikdp240@users.noreply.github.com>
2022-11-28 15:17:30 -0600

    Update databasescript_cs42projrect.sql

--- databasescript_cs42projrect.sql
+++ databasescript_cs42projrect.sql
@@ -4,6 +4,15 @@ DROP TABLE IF EXISTS Customer cascade;
 DROP TABLE IF EXISTS Transactions cascade;
 DROP TABLE IF EXISTS Account cascade;
 DROP TABLE IF EXISTS Loan cascade;
+
+CREATE TABLE Address (
+  address_id serial PRIMARY KEY,
+  house_num int NOT NULL,
+  street text NOT NULL,
+  city text NOT NULL,
+  state text NOT NULL,
+  zip char(5) NOT NULL
+);
  
 CREATE TABLE Employee (
   emp_ID serial UNIQUE,
@@ -11,77 +20,58 @@ CREATE TABLE Employee (
   first_name varchar NOT NULL,
   last_name varchar NOT NULL,
   salary numeric NOT NULL,
-  street varchar NOT NULL,
-  house_num int NOT NULL,
-  city varchar NOT NULL,
-  state varchar NOT NULL, 
-  zip char(5) NOT NULL,
   emp_role varchar NOT NULL,
+  lives_at int REFERENCES Address(address_id),
   works_at int REFERENCES Branch(branch_id),
   PRIMARY KEY (emp_ID, ssn),
-  CONSTRAINT emp_role_check CHECK (emp_role IN ('Manager', 'Teller', 'Loan Specialist'))
+  CONSTRAINT emp_role_check CHECK (emp_role IN ('Manager', 'Bank Teller', 'Loan Specialist'))
  );
 
 CREATE TABLE Branch (
-    branch_id serial PRIMARY KEY,
-    street varchar NOT NULL,
-    house_num int NOT NULL,
-    city varchar NOT NULL,
-    state varchar NOT NULL,
-    zip char(5),
-    --emp_ID int REFERENCES Employee ON DELETE CASCADE --I don't think we need this. employee and branch is referencing each other
+  branch_id serial PRIMARY KEY,
+  branch_at int REFERENCES Address(address_id)
 );
 
 CREATE TABLE Customer (
-    customer_id serial,
-    home_branch int REFERENCES Branch(branch_id),
-    firstName varchar NOT NULL,
-    lastName varchar NOT NULL,
-    street varchar NOT NULL,
-    house_num int NOT NULL,
-    city varchar NOT NULL,
-    state varchar NOT NULL,
-    zip char(5) NOT NULL,
-    account_num int,
-    PRIMARY KEY (customer_id),
-    --FOREIGN KEY (account_num) REFERENCES Account -- same reason as above / we can either make account references customer or customer references account
+  customer_id serial PRIMARY KEY,
+  home_branch int REFERENCES Branch(branch_id),
+  firstName varchar NOT NULL,
+  lastName varchar NOT NULL,
+  lives_at int REFERENCES Address(address_id),
+  account_num int,
 );
 
 CREATE TABLE Account (
-    account_id int REFERENCES Customer(customer_id) ON DELETE CASCADE,
-    account_num int PRIMARY KEY,
-    account_type varchar NOT NULL,
-    balance int NOT NULL,
-    CONSTRAINT account_type_check CHECK (account_type IN ('Checking', 'Savings'))
+   account_id int REFERENCES Customer(customer_id) ON DELETE CASCADE,
+   account_num int PRIMARY KEY,
+   balance int NOT NULL
 );
 
-CREATE TABLE accountType ( --specify details of different account types
-    account_type varchar,
-    interest_rate int NOT NULL,
-    allow_neg boolean NOT NULL DEFAULT FALSE,
-    overdraft_fee int NOT NULL,
-    monthly_fee int NOT NULL,
-    account_num int REFERENCES Account, -- would we need to add this to specify whose account we are talking about. - yes we need it for foreign key
-    PRIMARY KEY (account_type)
+CREATE TABLE AccountType (
+   account_type varchar PRIMARY KEY,
+   interest_rate int DEFAULT NULL,
+   allow_neg boolean DEFAULT FALSE,
+   overdraft_fee int DEFAULT NULL,
+   monthly_fee int DEFAULT NULL,
+   account_num int REFERENCES Account ON DELETE CASCADE,
+   CONSTRAINT account_type_check CHECK (account_type IN ('Checkings', 'Savings'))
 );
 
 
 CREATE TABLE Transactions (
-    description text,
-    amount int NOT NULL,
-    trans_type varchar NOT NULL,
-    account_num int REFERENCES Account,
-    CONSTRAINT transaction_type_check CHECK (trans_type IN ('Deposit', 'Withdrawal', 'Transfer', 'External Transfer'))
+   description text,
+   amount int NOT NULL,
+   trans_type varchar NOT NULL,
+   account_num int REFERENCES Account,
+   CONSTRAINT transaction_type_check CHECK (trans_type IN ('Deposit', 'Withdrawal', 'Transfer', 'External Transfer'))
 );
 
 CREATE TABLE Loan (
-    loan_amount int,
-    start_date date,
-    end_date date,
-    interest_schedule int,
-    account_num int,
-    PRIMARY KEY (loan_amount, start_date, end_date, interest_schedule),
-    FOREIGN KEY (account_num) REFERENCES Account
+   loan_amount int,
+   start_date date,
+   end_date date,
+   interest_schedule int,
+   account_num int,
+   PRIMARY KEY (loan_amount, start_date, end_date, interest_schedule),
+   FOREIGN KEY (account_num) REFERENCES Account
 );
-
-
