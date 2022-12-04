0f443263ce2b90cd301af298c385f396da1349c7
Erik Pacheco <92812258+Erikdp240@users.noreply.github.com>
2022-12-03 23:17:17 -0600

    Create insertdata.sql

---
+++ insertdata.sql
@@ -0,0 +1,17 @@
+-- This is just starter values for employees and branches
+
+INSERT INTO Address VALUES
+(1, '4959 W. North Ave.', 'Chicago', 'IL', '60639'),
+(2, '4900 W. George St.', 'Chicago', 'IL', '60641'),
+(3, '1300 N. Laramie Ave.', 'Chicago', 'IL', '60613'),
+(4, '6969 W. Irving Park Rd.', 'Chicago', 'IL', '60615'),
+(5, '4300 N. 31st St.', 'Chicago', 'IL', '60614');
+
+INSERT INTO Branch VALUES
+(1, 1),
+(2, 4);
+
+INSERT INTO Employee VALUES
+('ABC1234', 123456789, 'example123', 'Harsha', 'Pillai', 130000, 'Manager', 2, 1),
+('A506790', 908908003, 'password', 'Erik', 'Pacheco', 45000, 'Bank Teller', 3, 1),
+('L908769', 303789023, 'examplepass', 'Jungwoo', 'Hwang', 180000, 'Manager', 5, 4);
