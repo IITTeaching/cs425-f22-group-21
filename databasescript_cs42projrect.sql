8cc5b71ec5aef2d0f30e6a935e9b63b19046200f
Erik Pacheco <92812258+Erikdp240@users.noreply.github.com>
2022-12-06 22:01:02 -0600

    Added pending

--- databasescript_cs42projrect.sql
+++ databasescript_cs42projrect.sql
@@ -64,7 +64,8 @@ CREATE TABLE Transactions (
    amount int NOT NULL,
    trans_type text NOT NULL,
    account_id int REFERENCES Account,
-   transaction_date timestamp NOT NULL
+   transaction_date timestamp NOT NULL,
+   pending boolean DEFAULT TRUE,
    CONSTRAINT transaction_type_check CHECK (trans_type IN ('Deposit', 'Withdrawal', 'Transfer', 'External Transfer'))
 );
 
